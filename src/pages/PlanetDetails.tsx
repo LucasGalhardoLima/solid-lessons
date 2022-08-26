6f8acf1bfcf60c811bc788d5dbe4721dfaf9dc31
Lucas Galhardo Lima <lima.galhardo@gmail.com>
2022-08-26 18:39:30 -0300

    feat: adicao de listagem de detalhes do planeta

--- src/pages/PlanetDetails.tsx
+++ src/pages/PlanetDetails.tsx
@@ -1,10 +1,47 @@
-import { Component } from "solid-js";
+import { useParams } from "@solidjs/router";
+import { Component, onMount, Show } from "solid-js";
+import { listPlanet, planet } from "../stores/Planets";
 
 const PlanetDetails: Component = () => {
+    const params = useParams();
+
+    onMount(() => {
+        listPlanet(params.id);
+    });
+
+    console.log(planet)
     return (
-        <>
-            <h1>Details</h1>
-        </>
+        <Show
+            when={Object.keys(planet).length !== 0}
+            fallback={<div>Loading...</div>}
+        >
+            <div style="display: flex; justify-content: center; padding 20px">
+                <div style="margin: 5px; border: 1px solid #bdbdbd; padding: 5px;">
+                    <p> Name: {planet?.name}</p>
+                    <p> Climate: {planet?.climate}</p>
+                    <p> Terrain: {planet?.terrain}</p>
+                    <p> Diameter: {planet?.diameter}</p>
+                    <p> Population: {planet?.population}</p>
+                    <p> Gravity: {planet?.gravity}</p>
+                    <p>
+                        {" "}
+                        *Rotation Period: {planet?.rotation_period} <br />
+                        <span style="font-size: 10px">
+                            *The number of standard hours it takes for this
+                            planet to complete a single rotation on its axis
+                        </span>
+                    </p>
+                    <p>
+                        {" "}
+                        *Orbital Period: {planet?.orbital_period} <br />
+                        <span style="font-size: 10px">
+                            *The number of standard days it takes for this
+                            planet to complete a single orbit of its local star.
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </Show>
     );
 };
 
--- src/stores/Planets.tsx
+++ src/stores/Planets.tsx
@@ -4,6 +4,17 @@ import { mande } from "mande";
 
 const api = mande("https://swapi.dev/api/");
 
+interface Planet {
+    name?: string;
+    climate?: string;
+    terrain?: string;
+    diameter?: string;
+    population?: string;
+    gravity?: string;
+    rotation_period?: string;
+    orbital_period?: string;
+}
+
 export const listPlanets = async (search?: string) => {
     try {
         setPlanets([]);
@@ -12,16 +23,28 @@ export const listPlanets = async (search?: string) => {
             `planets/${search ? `?search=${search}` : ""}`
         );
         console.log(results);
-        setPlanets(results.map((item: { url: string; }) => ({
-          ...item,
-          id: item.url.substring(
-              item.url.length - 2,
-              item.url.length - 1
-          )
-        })));
+        setPlanets(
+            results.map((item: { url: string }) => ({
+                ...item,
+                id: item.url.substring(
+                    item.url.length - 2,
+                    item.url.length - 1
+                ),
+            }))
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const listPlanet = async (id: string) => {
+    try {
+        const response: object = await api.get(`/planets/${id}/`);
+        setPlanet({...response});
     } catch (error) {
         console.log(error);
     }
 };
 
 export const [planets, setPlanets] = createStore<Array<any>>([]);
+export const [planet, setPlanet] = createStore<Planet>({});
